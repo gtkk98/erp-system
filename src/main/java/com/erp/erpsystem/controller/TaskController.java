3f8cff52df9eb994b397300d4bb97b5eebaa42b1
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-23 21:51:19 +0530

    Create task controller for Http methods.

---
+++ src/main/java/com/erp/erpsystem/controller/TaskController.java
@@ -0,0 +1,31 @@
+package com.erp.erpsystem.controller;
+
+import com.erp.erpsystem.common.dto.request.TaskRequest;
+import com.erp.erpsystem.common.dto.response.TaskResponse;
+import com.erp.erpsystem.service.TaskService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskResponse createTask(
+            @Valid @RequestBody TaskRequest request) {
+        return taskService.createTask(request);
+    }
+
+    @GetMapping
+    public List<TaskResponse> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+}
